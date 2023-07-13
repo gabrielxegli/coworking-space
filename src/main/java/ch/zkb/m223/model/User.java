ace6fba4e41b28a841b5b4512ee11064ebeb7a9b
Gabriel Egli <mail@gabriel-egli.ch>
2023-07-13 07:29:04 +0000

    annotations and realtion done

--- src/main/java/ch/zkb/m223/model/User.java
+++ src/main/java/ch/zkb/m223/model/User.java
@@ -1,17 +1,48 @@
 package ch.zkb.m223.model;
 
+import java.util.Set;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
+import org.hibernate.annotations.Fetch;
+import org.hibernate.annotations.FetchMode;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@Entity
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Schema(readOnly = false)
     private Long id;
 
+    @Column(nullable = false)
     private String email;
 
+    @Column(nullable = false)
     private String password;
 
+    @Column(nullable = false)
     private String firstname;
 
+    @Column(nullable = false)
     private String lastname;
 
-    // private Role role;
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @OneToMany(mappedBy = "user")
+    @JsonIgnoreProperties("user")
+    @Fetch(FetchMode.JOIN)
+    private Set<Request> requests;
 
     public Long getId() {
         return this.id;
@@ -53,14 +84,12 @@ public class User {
         this.lastname = lastname;
     }
 
-    /*
-     * public Role getRole() {
-     * return this.role;
-     * }
-     * 
-     * public void setRole(Role role) {
-     * this.role = role;
-     * }
-     */
+    public Role getRole() {
+        return this.role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
 
 }
